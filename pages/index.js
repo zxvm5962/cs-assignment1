10b1991fc2562802612a7ffcbed4451dc3ef25ee
양병현 <quartz@PoQo-2.local>
2023-04-07 13:29:39 +0900

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import Link from 'next/link';
 
 export default function Home() {
   return (
@@ -11,11 +12,11 @@ export default function Home() {
 
       <main>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <Link href="/posts/first-page">B H Yang</Link>
         </h1>
 
         <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
+          I'm newbie 
         </p>
 
         <div className={styles.grid}>
---
+++ pages/posts/first-page.js
@@ -0,0 +1,10 @@
+export default function FirstPage() {
+    return (
+        <div>
+            23 INFO_SYS
+            <div>Yang Byung Hyun</div>
+            <div>15.01.99</div>
+            <div>Thank you!</div>
+        </div>
+    )
+}
